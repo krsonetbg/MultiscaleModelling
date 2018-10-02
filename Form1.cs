7ba896d22f7565f0f856b574a049ce6653890163
krsonetbg <krsonetbg@gmail.com>
2018-10-02 09:15:08 +0200

    Initial version

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiscaleModelling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button clicked...");
+            int dim = 150;
+            Bitmap bmp = new Bitmap(dim, dim);
+            for (int y = 0; y<dim; y++)
+            {
+                for (int x=0; x<dim; x++)
+                {
+                    bmp.SetPixel(y, x, Color.ForestGreen);
+                }
+            }
+            space_display.Image = bmp;
+
+        }
+    }
+}
---
+++ Neighborhood.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiscaleModelling
+{
+    class Neighborhood
+    {
+        public Neighborhood() { }
+        public void checkNeighborhood() { }
+    }
+}
